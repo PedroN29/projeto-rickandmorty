07e32b0398d2475c3d4fdafe6fd511c163507f13
Pedro Natanael Dias Abreu <126361534+PedroN29@users.noreply.github.com>
2023-06-09 22:13:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-rickandmorty
+ Projeto Rick and Morty
---
+++ index.js
@@ -0,0 +1,19 @@
+const btn1 = document.querySelector("#btntrocar")
+btn1.addEventListener('click', btntrocar)
+
+let img = 1
+let imagensarray = ['imagens/rick-e-morty.certa.jpg','imagens/abrindo-olho.certo.jpg', 'imagens/alienigenas-dandodedo.certo.jpg',
+'imagens/flutuando.certo.webp',
+'imagens/portal.certo.webp'];
+
+function btntrocar() {
+    if (img == imagensarray.length) {
+        img = 0
+    }
+    document.getElementById('img').src = imagensarray[img];
+    img++
+}
+
+
+
+//abrindo-olho.certo.jpg
